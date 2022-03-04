2d9ffd0f86d731d113ee0bb63cb72746cebf3bde
dunyamaliahmedli <tu7bg7w6d@code.edu.az>
2022-03-04 14:39:48 +0400

    added test file

---
+++ test/test/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string month = Console.ReadLine();
+               switch (month)
+            {
+                case "Yanvar":
+                case "May":
+                case "Mart":
+                case "Iyul":
+                case "Avqust":
+                case "Oktyabr":
+                case "Dekabr":
+                    Console.WriteLine("Bu ayda 31 gun var");
+                        break;
+
+                case "Aprel":
+                case "Iyun":
+                case "Sentyabr":
+                case "Noyabr":
+                    Console.WriteLine("Bu ayda 30 gun var");
+                        break;
+                case "Fevral":
+                    Console.WriteLine("Bu ay 4 ilden bir 28-29 gun olur");
+                        break;
+                default:
+                    Console.WriteLine("seni alladiblar bele ay yoxdur");
+                    break;
+
+
+                        
+
+            }
+                }
+    }
+}
