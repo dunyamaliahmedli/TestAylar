231a1909912425563af45b40f1005518f1a8bc93
dunyamaliahmedli <tu7bg7w6d@code.edu.az>
2022-03-04 15:49:59 +0400

    added test1

---
+++ test1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int m = Convert.ToInt32(Console.ReadLine());
+            int n = Convert.ToInt32(Console.ReadLine());
+            int count = 0;
+
+
+            for (int i = m; i <= n; i++)
+            {
+                if (i % 7 == 0&& i!=0)  
+                {
+                    count++;
+                }
+
+                   
+                        
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
